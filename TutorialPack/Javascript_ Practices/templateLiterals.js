57d85e1f7a22d5ae00b3c10cad0279ee3599db39
cornelius <corneliusakinloye071034@gmail.com>
2019-07-18 12:42:34 +0100

    Interpolation in template literals

--- TutorialPack/Javascript_ Practices/templateLiterals.js
+++ TutorialPack/Javascript_ Practices/templateLiterals.js
@@ -3,3 +3,5 @@
 let myStr = 'to see this happening Live';
 
 console.log(`I am waiting ${myStr}`);
+
+console.log(`I am waiting \${myStr}`); //By escaping the dollar sign, no interpolation takes place
