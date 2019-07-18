b31b224927b2caa5bb56ba59f8a71e3ba37d4bf5
cornelius <corneliusakinloye071034@gmail.com>
2019-07-18 12:58:25 +0100

    template literals with Rest Array

--- TutorialPack/Javascript_ Practices/templateLiterals.js
+++ TutorialPack/Javascript_ Practices/templateLiterals.js
@@ -5,3 +5,26 @@ let myStr = 'to see this happening Live';
 console.log(`I am waiting ${myStr}`);
 
 console.log(`I am waiting \${myStr}`); //By escaping the dollar sign, no interpolation takes place
+
+
+
+console.log(`I am waiting ${ "______" + myStr}`);
+
+
+
+function processInvoice(message){
+    console.log (message)
+}
+
+processInvoice `invoces`
+
+
+function processInvoice(segment, ...values){ //using Rest here 
+    console.log (segment)
+
+    console.log (values)
+}
+
+let segment = 'Adex';
+let values = 20;
+processInvoice `Invoices: ${segment} for ${values}`
